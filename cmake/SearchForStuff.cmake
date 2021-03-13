e4f9db1125b24c1e25d1aa713b56cf391a66de86
Florin9doi <Florin9doi@users.noreply.github.com>
2021-03-13 14:39:34 +0200

    Move des to 3rdparty

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -80,6 +80,7 @@ endif()
 
 set(CMAKE_FIND_FRAMEWORK ${FIND_FRAMEWORK_BACKUP})
 
+add_subdirectory(3rdparty/des)
 add_subdirectory(3rdparty/fast_float EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/lzma EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/libchdr EXCLUDE_FROM_ALL)
